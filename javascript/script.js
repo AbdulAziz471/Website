9581e8a71416b263fcee21ea92d272640cf7e102
@AbdulAziz471 <aziztariq2211@gmail.com>
2023-08-17 19:57:47 +0500

    added

--- javascript/script.js
+++ javascript/script.js
@@ -130,3 +130,10 @@ function changeImage(imageSource) {
 }
 
 
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
